03be5c1d1fa299c24fb72c1f5d76536436e3dac8
davronmamaraimov1992@gmail.com <davronmamaraimov1996@gmail.com>
2023-10-05 15:46:25 +0500

    Header done

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "./style.css";
+
+class Footer extends Component {
+  render() {
+    return <div className="footer"></div>;
+  }
+}
+
+export default Footer;
---
+++ src/components/Middle.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "./style.css";
+
+class Middle extends Component {
+  render() {
+    return <div className="middle"></div>;
+  }
+}
+
+export default Middle;

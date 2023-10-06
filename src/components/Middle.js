332c9637c8b2e28693e7554f1119a2601badd9ba
davronmamaraimov1992@gmail.com <davronmamaraimov1996@gmail.com>
2023-10-06 14:33:49 +0500

    Middle done

--- src/components/Middle.js
+++ src/components/Middle.js
@@ -77,9 +77,64 @@ class Middle extends Component {
                 </div>
               </div>
             </div>
+
+            <div className="pagination">
+              <div className="newer">Newer</div>
+              <div>
+                <a href="!#">1</a>
+              </div>
+              <div>
+                <a href="!#">2</a>
+              </div>
+              <div>
+                <a href="!#">3</a>
+              </div>
+              <div>
+                <a href="!#">...</a>
+              </div>
+              <div>
+                <a href="!#">15</a>
+              </div>
+              <div id="older">
+                <a href="!#">Older</a>
+              </div>
+            </div>
           </div>
         </div>
-        <div className="right-box"></div>
+
+        <div className="right-box">
+          <div className="search-box">
+            <span>Search</span>
+            <div className="input">
+              <input type="text" placeholder="Enter search term..." />
+              <button className="btn btn-primary">Go!</button>
+            </div>
+          </div>
+
+          <div className="categories">
+            <span>Categories</span>
+            <div className="programming-languages">
+              <div className="box-1">
+                <a href="!#">Web Design</a>
+                <a href="!#">HTML</a>
+                <a href="!#">Freebies</a>
+              </div>
+              <div className="box-2">
+                <a href="!#">Javascript</a>
+                <a href="!#">CSS</a>
+                <a href="!#">Tutorials</a>
+              </div>
+            </div>
+          </div>
+
+          <div className="side-widget">
+            <span>Side Widget</span>
+            <p>
+              You can put anything you want inside of these side widgets. They
+              are easy to use, and feature the Bootstrap 5 card component!
+            </p>
+          </div>
+        </div>
       </div>
     );
   }
